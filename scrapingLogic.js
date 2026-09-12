0ea9778e7d08b5b85ef0ba57e50af673e7abed9e
ryn-01 <ryanandrewlee558@gmail.com>
2026-09-12 20:13:46 +0700

    puppeteer turn on headless

--- scrapingLogic.js
+++ scrapingLogic.js
@@ -15,7 +15,7 @@ async function getBrowser() {
     return browserInstance
   }
 
-  browserInstance = await puppeteer.launch({ headless: false, defaultViewport: null, })
+  browserInstance = await puppeteer.launch({ headless: true, defaultViewport: null, })
 }
 
 const methods = {
