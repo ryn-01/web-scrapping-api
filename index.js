106199a543950eab0564698acfb1ed26b336affc
ryn-01 <ryanandrewlee558@gmail.com>
2026-09-12 18:08:22 +0700

    Added Request Limit & Single Browser Instance

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import CompileInstruction from "./scrapingLogic.js";
 import express from "express"
+import rateLimit from "express-rate-limit";
+
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes in milliseconds
+  limit: 5,               // Limit each IP to 5 requests per windowMs
+  standardHeaders: 'draft-7', // return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false,     // Disable the older `X-RateLimit-*` headers
+  message: 'Too many requests from this IP, please try again later.',
+});
 
 const app = express();
 app.use(express.json());
+app.use(globalLimiter);
+
 const PORT = 3000;
 
 app.post("/api/data", async(req, res) => {
--- scrapingLogic.js
+++ scrapingLogic.js
@@ -5,103 +5,100 @@ import AdblockerPlugin from "puppeteer-extra-plugin-adblocker";
 puppeteer.use(StealthPlugin());
 puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
 
+let browserInstance = null;
+
+async function getBrowser() {
+  if (browserInstance && browserInstance.connected) {
+    return browserInstance
+  }
+
+  browserInstance = await puppeteer.launch({ headless: false, defaultViewport: null, })
+}
+
+const methods = {
+  goto: async (page, selector) =>
+    await page.goto(selector, { waitUntil: "networkidle2" }),
+
+  waitFor: async (page, selector) => await page.waitForSelector(selector),
+
+  search: async (page, data) => {
+    await page.locator(data.selector).fill(data.query);
+    await page.keyboard.press("Enter");
+  },
+
+  clickBtn: async (page, selector) => await page.locator(selector).click(),
+
+  clickLnk: async (page, selector) => {
+    const link = await page.$eval(selector, (el) => el.href);
+    await page.goto(link, { waitUntil: "networkidle2" });
+  },
+
+  input: async (page, data) =>
+    await page.locator(data.selector).fill(data.query),
+
+  extract: async (page, data, collectedData) => {
+    await page.waitForSelector(data.selector);
+    collectedData.push(await page.$eval(
+      data.selector,
+      (el, selectedAtrb) => {
+        let selectedData = {};
+        selectedAtrb.forEach((atrb) => {
+          selectedData[atrb] = el[atrb];
+        });
+        return selectedData;
+      },
+      data.atributes,
+    ))
+  },
+
+  extractAll: async (page, data, collectedData) => {
+    await page.waitForSelector(data.selector);
+
+    collectedData.push(await page.$$eval(
+      data.selector,
+      (els, selectedAtrb) => {
+        let result = [];
+        els.forEach((el, index) => {
+          let selectedData = {};
+          for (let i = 0; i < selectedAtrb.length; i++) {
+            let atrbName = selectedAtrb[i];
+            selectedData[atrbName] = el[atrbName];
+          }
+          result.push(selectedData);
+        });
+        return result;
+      },
+      data.atributes,
+    ))
+  },
+
+  extractTable: async (page, selector, collectedData) => {
+    await page.waitForSelector(selector);
+    collectedData.push(await page.$$eval(selector, (rows) => {
+      return rows.map((row) =>
+        [...row.querySelectorAll("th, td")].map((cell) =>
+          cell.innerText.trim(),
+        ),
+      );
+    }))
+  },
+};
+
 async function CompileInstruction(list) {
+  await getBrowser()
+  const page = await browserInstance.newPage();
   const collectedData = [];
-
-  const method = {
-    goto: async (page, selector) =>
-      await page.goto(selector, { waitUntil: "networkidle2" }),
-
-    waitFor: async (page, selector) => await page.waitForSelector(selector),
-
-    search: async (page, data) => {
-      await page.locator(data.selector).fill(data.query);
-      await page.keyboard.press("Enter");
-    },
-
-    clickBtn: async (page, selector) => await page.locator(selector).click(),
-
-    clickLnk: async (page, selector) => {
-      const link = await page.$eval(selector, (el) => el.href);
-      await page.goto(link, { waitUntil: "networkidle2" });
-    },
-
-    input: async (page, data) =>
-      await page.locator(data.selector).fill(data.query),
-
-    extract: async (page, data) => {
-      await page.waitForSelector(data.selector);
-
-      collectedData.push(
-        await page.$eval(
-          data.selector,
-          (el, selectedAtrb) => {
-            let selectedData = {};
-            selectedAtrb.forEach((atrb) => {
-              selectedData[atrb] = el[atrb];
-            });
-            return selectedData;
-          },
-          data.atributes,
-        ),
-      );
-    },
-
-    extractAll: async (page, data) => {
-      await page.waitForSelector(data.selector);
-
-      collectedData.push(
-        await page.$$eval(
-          data.selector,
-          (els, selectedAtrb) => {
-            let result = [];
-            els.forEach((el, index) => {
-              let selectedData = {};
-
-              for (let i = 0; i < selectedAtrb.length; i++) {
-                let atrbName = selectedAtrb[i];
-                selectedData[atrbName] = el[atrbName];
-              }
-              result.push(selectedData);
-            });
-            return result;
-          },
-          data.atributes,
-        ),
-      );
-    },
-
-    extractTable: async (page, selector) => {
-      await page.waitForSelector(selector);
-
-      collectedData.push(
-        await page.$$eval(selector, (rows) => {
-          return rows.map((row) =>
-            [...row.querySelectorAll("th, td")].map((cell) =>
-              cell.innerText.trim(),
-            ),
-          );
-        }),
-      );
-    },
-  };
-
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: null,
-  });
-  const page = await browser.newPage();
 
   try {
     for (const step of list) {
-      const func = method[step.action];
-      if (!func) throw new Error(`Unknown action: ${method[step.action]}`);
-      await func(page, step.value);
+      const func = methods[step.action];
+      if (!func) throw new Error(`Unknown action: ${step.action}`);
+      await func(page, step.value, collectedData);
     }
   } catch (err) {
     return { success: false, error: err.message };
   } finally {
-    await browser.close();
+    await page.close()
   }
 
   return { success: true, data: collectedData };
